2f2984ddd8f18335ed940b6297c2eb72719cf2d5
Federico Marmo <federico.marmo.1990@gmail.com>
2022-01-21 20:55:21 -0300

    remove unused app folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const stylesHandler = "style-loader";
 
 const config = {
-  entry: "./app/main.ts",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
