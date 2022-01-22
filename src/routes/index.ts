f88c875c1938c7699905d59118846daa314ae82b
Federico Marmo <federico.marmo.1990@gmail.com>
2022-01-22 18:07:29 -0300

    add test for server functionality

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,21 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
-const responseJson = (res: ServerResponse, response: object) : void => {
+const responseJson = (res: ServerResponse, response: object, statusCode: number) : void => {
     res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200);
+    res.writeHead(statusCode);
     res.end(JSON.stringify(response));
 };
 
 const routing = (req: IncomingMessage, res: ServerResponse) : void => {
     if (req.url === '/') {
-        responseJson(res, {
+        return responseJson(res, {
             hello: 'world'
-        });
+        }, 200);
     }
+
+    return responseJson(res, {
+        error: 'Route not found'
+    }, 404);
 };
 
 export default routing;
---
+++ test/unit/main.test.ts
@@ -0,0 +1,21 @@
+import { ServerResponse } from 'http';
+import server from '../../src/app/server';
+import request from 'supertest';
+
+describe('Testing http server functionality', () => {
+  it('The main route return valid response', async (done: Function) : Promise<void> => {
+    const res: ServerResponse = await request(server).get('/');
+  
+    expect(res.statusCode).toBe(200);
+  
+    done();
+  });
+
+  it('The server response is 404 when given route is not found', async (done: Function) : Promise<void> => {
+    const res: ServerResponse = await request(server).get('/notValidRoute');
+
+    expect(res.statusCode).toBe(404);
+    
+    done();
+  });
+});
