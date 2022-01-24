8f03e0bb7f3f0d6a82281b222255268a07921c2b
Federico Marmo <federico.marmo.1990@gmail.com>
2022-01-23 21:07:20 -0300

    add unique name generator for files + new ts types

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -5,6 +5,7 @@ import fs from 'fs';
 import appRoot from 'app-root-path';
 import csv from'csv-parser';
 import { UserService } from './userServices';
+import uniqueFileName from 'unique-filename';
 
 export class FileService {
     static saveUpload(req: IncomingMessage) : Promise<UserInputData> {
@@ -22,7 +23,7 @@ export class FileService {
                     fs.mkdirSync(appRoot + '/dist/uploads/');
                 }
     
-                const newPath = appRoot + '/dist/uploads/' + files.csvFile.originalFilename;
+                const newPath = uniqueFileName(appRoot + '/dist/uploads/', 'file') + '.csv';
     
                 fs.rename(oldPath, newPath, function (err) {
                     if (err) {
@@ -31,15 +32,15 @@ export class FileService {
 
                     resolve({
                         phone: fields.phone,
-                        initDate: fields.initDate,
-                        endDate: fields.endDate,
-                        fileName: files.csvFile.originalFilename
+                        initDate: String(fields.initDate).replace(/-/g, ''),
+                        endDate: String(fields.endDate).replace(/-/g, ''),
+                        fileName: newPath
                     });
                 });
             });
         })
     }
-
+    
     static async process(userInputData: UserInputData) {
         if (!userInputData.phone) {
             throw Error('No phone for user');
@@ -48,8 +49,10 @@ export class FileService {
         try {
             const userInfo: User = await UserService.getInfo(userInputData.phone);
 
+            let count = 0;
+
             return new Promise((resolve, reject) => {
-                const filePath = appRoot + `/dist/uploads/${userInputData.fileName}`;
+                const filePath = userInputData.fileName;
 
                 fs.createReadStream(filePath)
                 .pipe(csv({
@@ -61,19 +64,53 @@ export class FileService {
                         'date'
                     ]
                 }))
-                .on('data', (data) => {
-                    if (data.origin === userInputData.phone) {
-                        //console.log(data);
+                .on('data', (data: CSVData) => {
+                    if (data.origin === userInputData.phone && data.destination !== userInputData.phone) {
+                        data.date = data.date.split('T')[0].replace(/-/g, '');
+
+                        if (data.date >= userInputData.initDate && data.date <= userInputData.endDate) {
+                            this.formarData(data, userInfo);
+
+                            //console.log(data);
+                            count++;
+                        }
                     }
                 })
                 .on('end', () => {
                     fs.unlinkSync(filePath);
-                    console.log('finish');
+                    console.log(`finish, process ${count} fields`);
+                    count = 0;
                     resolve(true);
                 });
             });
         } catch (err) {
             throw err;
+        }
+    }
+
+    static formarData(data: CSVData, userInfo: User) {
+        this.formatDuration(data);
+        this.formatCallType(data, userInfo);
+    }
+
+    static formatDuration(data: CSVData) {
+        data.duration = Math.floor(Number(data.duration) / 60);
+    }
+
+    static formatCallType(data: CSVData, userInfo: User) {
+        let countryOrigin = data.origin.substring(1,3);
+        let countryDestination = data.destination.substring(1,3);
+
+        if (countryOrigin === countryDestination) {
+            data.type = 'N';
+        }
+
+        if (countryOrigin !== countryDestination) {
+            data.type = 'I';
+        }
+
+        if (userInfo.firendsList.find(element => element === data.destination)) {
+            data.type = 'F';
         }
     }
 }
--- src/types/commons.d.ts
+++ src/types/commons.d.ts
@@ -21,3 +21,29 @@ interface User {
 interface UserFriend {
     phone: string
 }
+
+interface Invoice {
+    user: User,
+    movements: Array<Movement>,
+    totalCharged: string,
+    totalNationalMinutes: string,
+    totalInternationalMinutes: string,
+    totalFriendsMinutes: string
+}
+
+interface Movement {
+    destination: string,
+    origin: string,
+    duration: string,
+    date: string,
+    price: string
+}
+
+interface CSVData {
+    origin: string,
+    destination: string,
+    revert: string,
+    duration: string | number,
+    date: string,
+    type?: string
+}
