9032b1325e0126c4a1aa091d68cf51df7c4de590
Federico Marmo <federico.marmo.1990@gmail.com>
2022-01-25 20:26:33 -0300

    fix cache usage name

--- src/interface/adapter/rest/userAdapter.ts
+++ src/interface/adapter/rest/userAdapter.ts
@@ -3,23 +3,22 @@ import fetch from 'node-fetch';
 import config from 'config';
 import { RedisAdapter } from '../cache/redisAdapter';
 
-
 export default class UserAdapter implements RestAdapter {
     async get(phone: string) {
         try {
             let apiResult: UserApiResponse = null;
 
-            const redisCache = new RedisAdapter();
+            const cache = new RedisAdapter();
 
-            if (await redisCache.get(phone)) {
-                apiResult = JSON.parse(await redisCache.get(phone));
+            if (await cache.get(phone)) {
+                apiResult = JSON.parse(await cache.get(phone));
             } else {
                 const apiUrl = config.get('dependencies.userApi.url').replace('#phone', encodeURI(phone));
                 const response: any = await fetch(apiUrl);
                 
                 apiResult = await response.json();
 
-                await redisCache.save(phone, apiResult, 300);
+                await cache.save(phone, apiResult, 300);
             }
 
             return {
