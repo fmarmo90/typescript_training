577d66f4d18401f0af717fcdd78af1f68005b43a
Federico Marmo <federico.marmo.1990@gmail.com>
2022-01-25 14:39:52 -0300

    add port from config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import server from './infrastructure/server';
+import config from 'config';
 
-const PORT: number = 3000;
+const PORT: number = config.get('port') || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
